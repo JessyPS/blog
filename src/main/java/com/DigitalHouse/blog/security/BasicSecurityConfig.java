cf0c89a851a9fc2a8906cee1eeeeccb6c2e23dc7
JessyPS <89113356+JessyPS@users.noreply.github.com>
2021-10-29 08:58:46 -0300

    testando heroku com alterações

--- src/main/java/com/DigitalHouse/blog/security/BasicSecurityConfig.java
+++ src/main/java/com/DigitalHouse/blog/security/BasicSecurityConfig.java
@@ -33,7 +33,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/usuario/logar").permitAll()
                 .antMatchers("/usuario/cadastrar").permitAll()
-                .anyRequest().authenticated()
+//                .anyRequest().authenticated()
                 .and().httpBasic()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().cors()
